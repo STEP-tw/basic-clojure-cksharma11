2b3ccdf5f7ee3f1ea0e030c3325cd13f2ae2fd34
Chandan Kumar <cksharma122@gmail.com>
2020-02-05 17:22:23 +0530

    Fixed indentation

--- src/assignments/conditions.clj
+++ src/assignments/conditions.clj
@@ -1,146 +1,146 @@
 (ns assignments.conditions)
 
 (defn safe-divide
-  "Returns the result of x/y unless y is 0. Returns nil when y is 0"
-  {:level        :easy
-   :use          '[when-not zero?]
-   :implemented? true}
-  [x y]
-	(when-not (zero? y) (/ x y)))
+ "Returns the result of x/y unless y is 0. Returns nil when y is 0"
+ {:level        :easy
+	:use          '[when-not zero?]
+	:implemented? true}
+ [x y]
+ (when-not (zero? y) (/ x y)))
 
 (defn informative-divide
-  "Returns the result of x/y unless y is 0. Returns :infinite when y is 0"
-  {:level        :easy
-   :use          '[if-not zero?]
-   :implemented? true}
-  [x y]
-	(if-not (zero? y) (/ x y) :infinite))
+ "Returns the result of x/y unless y is 0. Returns :infinite when y is 0"
+ {:level        :easy
+	:use          '[if-not zero?]
+	:implemented? true}
+ [x y]
+ (if-not (zero? y) (/ x y) :infinite))
 
 (defn harishchandra
-  "Only returns truthy values as themselves.
-  Falsy values(false and nil) return nil"
-  {:level        :easy
-   :use          '[when-let]
-   :implemented? true}
-  [x]
-	(when-let [y x] y))
+ "Only returns truthy values as themselves.
+ Falsy values(false and nil) return nil"
+ {:level        :easy
+	:use          '[when-let]
+	:implemented? true}
+ [x]
+ (when-let [y x] y))
 
 (defn yudishtira
-  "Only returns truthy values as themselves.
-  Falsy values(false and nil) return :ashwathama"
-  {:level        :easy
-   :use          '[if-let]
-   :implemented? true}
-  [x]
-	(if-let [y x] y :ashwathama))
+ "Only returns truthy values as themselves.
+ Falsy values(false and nil) return :ashwathama"
+ {:level        :easy
+	:use          '[if-let]
+	:implemented? true}
+ [x]
+ (if-let [y x] y :ashwathama))
 
 (defn duplicate-first
-  "Returns coll with the first element duplicated.
-  Returns nil if col is empty"
-  {:level        :easy
-   :use          '[when-first concat]
-   :alternates   '[empty? seq? conj into]
-   :implemented? true}
-  [coll]
-	(when-first [y coll] (into [y] coll)))
+ "Returns coll with the first element duplicated.
+ Returns nil if col is empty"
+ {:level        :easy
+	:use          '[when-first concat]
+	:alternates   '[empty? seq? conj into]
+	:implemented? true}
+ [coll]
+ (when-first [y coll] (into [y] coll)))
 
 (defn five-point-someone
-  "Returns :chetan-bhagat if y is 5.
-  If x is 5 is returns :satan-bhagat.
-  If x is greater than y it returns :greece
-  Otherwise it returns :universe"
-  {:level        :easy
-   :use          '[cond]
-   :implemented? true}
-  [x y]
-	(cond
-		(= y 5) :chetan-bhagat
-		(= x 5) :satan-bhagat
-		(> x y) :greece
-		:else :universe))
+ "Returns :chetan-bhagat if y is 5.
+ If x is 5 is returns :satan-bhagat.
+ If x is greater than y it returns :greece
+ Otherwise it returns :universe"
+ {:level        :easy
+	:use          '[cond]
+	:implemented? true}
+ [x y]
+ (cond
+	(= y 5) :chetan-bhagat
+	(= x 5) :satan-bhagat
+	(> x y) :greece
+	:else :universe))
 
 (defn is-equal-and-same-order [subset coll]
-	(-> (set subset)
-			(filter coll)
-			(= subset)))
+ (-> (set subset)
+		 (filter coll)
+		 (= subset)))
 
 (defn conditions-apply
-  "Given a collection of any length, returns:
-  :wonder-woman if collection has a single occurrence of 1 and 3 in that order
-  :durga        if collection has a single occurrence of :a :b and :c in that order
-  :cleopatra    if collection has a single occurrence of [2 3] and [4 5] in that order
-  :tuntun if none of the conditions apply"
-  {:level        :medium
-   :use          '[condp filter]
-   :alternates   '[if cond]
-   :implemented? true}
-  [coll]
-	(condp is-equal-and-same-order coll
-		[1 3] :wonder-woman
-		[:a :b :c] :durga
-		[[2 3] [4 5]] :cleopatra
-		:tuntun))
+ "Given a collection of any length, returns:
+ :wonder-woman if collection has a single occurrence of 1 and 3 in that order
+ :durga        if collection has a single occurrence of :a :b and :c in that order
+ :cleopatra    if collection has a single occurrence of [2 3] and [4 5] in that order
+ :tuntun if none of the conditions apply"
+ {:level        :medium
+	:use          '[condp filter]
+	:alternates   '[if cond]
+	:implemented? true}
+ [coll]
+ (condp is-equal-and-same-order coll
+	[1 3] :wonder-woman
+	[:a :b :c] :durga
+	[[2 3] [4 5]] :cleopatra
+	:tuntun))
 
 (defn repeat-and-truncate
-  "Given coll and options to repeat and truncate
-  returns a collection that optionally repeats itself
-  and is optionally truncated to the first n elements.
-  (repeat-and-truncate (range 4) true true 6) => '(0 1 2 3 0 1)"
-  {:level        :medium
-   :use          '[cond->> concat take]
-   :implemented? true}
-  [coll rep? truncate? n]
-	(cond->> coll
-					 rep? (concat coll)
-					 truncate? (take n)))
+ "Given coll and options to repeat and truncate
+ returns a collection that optionally repeats itself
+ and is optionally truncated to the first n elements.
+ (repeat-and-truncate (range 4) true true 6) => '(0 1 2 3 0 1)"
+ {:level        :medium
+	:use          '[cond->> concat take]
+	:implemented? true}
+ [coll rep? truncate? n]
+ (cond->> coll
+					rep? (concat coll)
+					truncate? (take n)))
 
 (defn order-in-words
-  "Given x, y and z, returns a vector consisting of
-  sentences that explicitly mention if x > y or y > z or z > x.
-  (order-in-words 4 3 2) => [:x-greater-than-y :y-greater-than-z]
-  (order-in-words 4 3 5) => [:x-greater-than-y :z-greater-than-x]
-  (order-in-words 2 3 4) => [:z-greater-than-x]"
-  {:level        :easy
-   :use          '[cond-> conj]
-   :implemented? true}
-  [x y z]
-	(cond-> []
-					(> x y) (conj :x-greater-than-y)
-					(> y z) (conj :y-greater-than-z)
-					(> z x) (conj :z-greater-than-x)))
+ "Given x, y and z, returns a vector consisting of
+ sentences that explicitly mention if x > y or y > z or z > x.
+ (order-in-words 4 3 2) => [:x-greater-than-y :y-greater-than-z]
+ (order-in-words 4 3 5) => [:x-greater-than-y :z-greater-than-x]
+ (order-in-words 2 3 4) => [:z-greater-than-x]"
+ {:level        :easy
+	:use          '[cond-> conj]
+	:implemented? true}
+ [x y z]
+ (cond-> []
+				 (> x y) (conj :x-greater-than-y)
+				 (> y z) (conj :y-greater-than-z)
+				 (> z x) (conj :z-greater-than-x)))
 
 (defn zero-aliases
-  "Given a zero-like value(0,[],(),#{},{}) should
-  give back an alias for each type of zero like value
-  or a default of :not-zero
-  0 -> :zero
-  [] -> :empty
-  '() -> :empty
-  #{} -> :empty-set
-  {}  -> :empty-map
-  \"\"  -> :empty-string"
-  {:level        :easy
-   :use          '[case]
-   :implemented? true}
-  [zero-like-value]
-	(case zero-like-value
-		(0) :zero
-		([]) :empty
-		('()) :empty
-		(#{}) :empty-set
-		({}) :empty-map
-		("") :empty-string
-		:non-zero))
+ "Given a zero-like value(0,[],(),#{},{}) should
+ give back an alias for each type of zero like value
+ or a default of :not-zero
+ 0 -> :zero
+ [] -> :empty
+ '() -> :empty
+ #{} -> :empty-set
+ {}  -> :empty-map
+ \"\"  -> :empty-string"
+ {:level        :easy
+	:use          '[case]
+	:implemented? true}
+ [zero-like-value]
+ (case zero-like-value
+	(0) :zero
+	([]) :empty
+	('()) :empty
+	(#{}) :empty-set
+	({}) :empty-map
+	("") :empty-string
+	:non-zero))
 
 (defn zero-separated-palindrome
-  "Given a sequence of numbers, increment the list
-  and prepend a 0 to the incremented list concatenated
-  with the reverse of the incremented list
-  [1 2 3] -> (4 3 2 0 2 3 4)"
-  {:level :easy
-   :use '[as-> reverse]
-   :implemented? true}
-  [coll]
-	(as-> coll x
-				(map inc x)
-				(concat (reverse x) (conj x 0))))
+ "Given a sequence of numbers, increment the list
+ and prepend a 0 to the incremented list concatenated
+ with the reverse of the incremented list
+ [1 2 3] -> (4 3 2 0 2 3 4)"
+ {:level        :easy
+	:use          '[as-> reverse]
+	:implemented? true}
+ [coll]
+ (as-> coll x
+			 (map inc x)
+			 (concat (reverse x) (conj x 0))))
--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -1,127 +1,127 @@
 (ns assignments.lists-test
-  (:require [clojure.test :refer :all]
-            [assignments.lists :refer :all]))
+ (:require [clojure.test :refer :all]
+					 [assignments.lists :refer :all]))
 
 (deftest lists
-  (testing "map"
-    (testing "identity with single coll"
-      (is (= [1 2 3] (map' identity [1 2 3]))))))
+ (testing "map"
+	(testing "identity with single coll"
+	 (is (= [1 2 3] (map' identity [1 2 3]))))))
 
 (deftest filters
-  (testing "filter"
-    (testing "filter with pred"
-      (is (= [1 3] (filter' odd? [1 2 3]))))))
+ (testing "filter"
+	(testing "filter with pred"
+	 (is (= [1 3] (filter' odd? [1 2 3]))))))
 
 (deftest reverses
-	(testing "valid sequence"
-		(is (= [3 2 1] (reverse' [1 2 3])))))
+ (testing "valid sequence"
+	(is (= [3 2 1] (reverse' [1 2 3])))))
 
 (deftest count-test
-	(testing "sequence length"
-		(is (= 5 (count' [1 2 3 4 5]))))
-	(testing "nil"
-		(is (= 0 (count' nil))))
-	(testing "[]"
-		(is (= 0 (count' []))))
-	(testing "map"
-		(is (= 2 (count {:one 1 :two 2}))))
-	(testing "string"
-		(is (= 6 (count "abcdef")))))
+ (testing "sequence length"
+	(is (= 5 (count' [1 2 3 4 5]))))
+ (testing "nil"
+	(is (= 0 (count' nil))))
+ (testing "[]"
+	(is (= 0 (count' []))))
+ (testing "map"
+	(is (= 2 (count {:one 1 :two 2}))))
+ (testing "string"
+	(is (= 6 (count "abcdef")))))
 
 (deftest reduce-test
-	(testing "arity 2"
-		(is (= 15 (reduce' + [1 2 3 4 5]))))
-	(testing "arity 3"
-		(is (= 15 (reduce' + 1 [2 3 4 5])))))
+ (testing "arity 2"
+	(is (= 15 (reduce' + [1 2 3 4 5]))))
+ (testing "arity 3"
+	(is (= 15 (reduce' + 1 [2 3 4 5])))))
 
 (deftest everys
-	(testing "with empty list"
-		(is (= true (every?' even? []))))
-	(testing "with all true values"
-		(is (= true (every?' even? [2 4 6]))))
-	(testing "with all falsy values"
-		(is (= false (every?' even? [1 3 5]))))
-	(testing "with some true values"
-		(is (= false (every?' even? [1 2])))))
+ (testing "with empty list"
+	(is (= true (every?' even? []))))
+ (testing "with all true values"
+	(is (= true (every?' even? [2 4 6]))))
+ (testing "with all falsy values"
+	(is (= false (every?' even? [1 3 5]))))
+ (testing "with some true values"
+	(is (= false (every?' even? [1 2])))))
 
 (deftest somes
-	(testing "with empty list"
-		(is (= false (some?' even? []))))
-	(testing "with all true values"
-		(is (= true (some?' even? [2 4 6]))))
-	(testing "with all falsy values"
-		(is (= false (some?' even? [1 3 5]))))
-	(testing "with some true values"
-		(is (= true (some?' even? [1 2])))))
+ (testing "with empty list"
+	(is (= false (some?' even? []))))
+ (testing "with all true values"
+	(is (= true (some?' even? [2 4 6]))))
+ (testing "with all falsy values"
+	(is (= false (some?' even? [1 3 5]))))
+ (testing "with some true values"
+	(is (= true (some?' even? [1 2])))))
 
 (deftest ascendings
-	(testing "with ascending list"
-		(is (= true (ascending? [1 2 3]))))
-	(testing "with not ascending list"
-		(is (= false (ascending? [2 14 6])))))
+ (testing "with ascending list"
+	(is (= true (ascending? [1 2 3]))))
+ (testing "with not ascending list"
+	(is (= false (ascending? [2 14 6])))))
 
 (deftest sum-of-adjacent-digits-test
-	(testing "with ascending list"
-		(is (= [3 5] (sum-of-adjacent-digits [1 2 3])))))
+ (testing "with ascending list"
+	(is (= [3 5] (sum-of-adjacent-digits [1 2 3])))))
 
 (deftest difference-test
-	(testing "different list"
-		(is (= '(4 5) (difference [1 2 3] [1 2 3 4 5]))))
-	(testing "same list"
-		(is (= '() (difference [1 2 3] [1 2 3])))))
+ (testing "different list"
+	(is (= '(4 5) (difference [1 2 3] [1 2 3 4 5]))))
+ (testing "same list"
+	(is (= '() (difference [1 2 3] [1 2 3])))))
 
 (deftest muted-thirds-test
-	(testing "different list"
-		(is (= '(1 2 0 4 5 0) (muted-thirds [1 2 3 4 5 6])))))
+ (testing "different list"
+	(is (= '(1 2 0 4 5 0) (muted-thirds [1 2 3 4 5 6])))))
 
 (deftest palindrome-test
-	(testing "valid palindrome"
-		(is (= true (palindrome? [1 2 3 2 1]))))
-	(testing "invalid palindrome"
-		(is (= false (palindrome? [ 1 2 3])))))
+ (testing "valid palindrome"
+	(is (= true (palindrome? [1 2 3 2 1]))))
+ (testing "invalid palindrome"
+	(is (= false (palindrome? [1 2 3])))))
 
 (deftest index-of-test
-	(testing "element present in collection"
-		(is (= 1 (index-of [1 2 3] 2))))
-	(testing "element not present in collection"
-		(is (= -1 (index-of [ 1 2 3] 4)))))
+ (testing "element present in collection"
+	(is (= 1 (index-of [1 2 3] 2))))
+ (testing "element not present in collection"
+	(is (= -1 (index-of [1 2 3] 4)))))
 
 (deftest russian-dolls-test
-	(testing "[1 2 3] 3 => [[[1]] [[2]] [[3]]]"
-		(is (= [[[1]] [[2]] [[3]]]  (russian-dolls [1 2 3] 3)))))
+ (testing "[1 2 3] 3 => [[[1]] [[2]] [[3]]]"
+	(is (= [[[1]] [[2]] [[3]]] (russian-dolls [1 2 3] 3)))))
 
 (deftest split-comb-test
-	(testing "[1 2 3 4 5] => [1 3 2 4 5]"
-		(is (= [1 3 2 4 5]  (split-comb [1 2 3 4 5])))))
+ (testing "[1 2 3 4 5] => [1 3 2 4 5]"
+	(is (= [1 3 2 4 5] (split-comb [1 2 3 4 5])))))
 
 (deftest double-up-test
-	(testing ""
-		(is (= [1 1 2 2]  (double-up [1 2])))))
+ (testing ""
+	(is (= [1 1 2 2] (double-up [1 2])))))
 
 (deftest cross-product-test
-	(testing ""
-		(is (= [[1 4] [1 3] [1 5] [2 4] [2 3] [2 5] [3 4]]  (cross-product [1 2 3] [4 3 5])))))
+ (testing ""
+	(is (= [[1 4] [1 3] [1 5] [2 4] [2 3] [2 5] [3 4]] (cross-product [1 2 3] [4 3 5])))))
 
 (deftest validate-sudoku-grid-test
-	(testing "correct grid"
-		(is (true? (validate-sudoku-grid
-								 [[4 3 5 2 6 9 7 8 1]
-									[6 8 2 5 7 1 4 9 3]
-									[1 9 7 8 3 4 5 6 2]
-									[8 2 6 1 9 5 3 4 7]
-									[3 7 4 6 8 2 9 1 5]
-									[9 5 1 7 4 3 6 2 8]
-									[5 1 9 3 2 6 8 7 4]
-									[2 4 8 9 5 7 1 3 6]
-									[7 6 3 4 1 8 2 5 9]]))))
-	(testing "incorrect grid"
-		(is (false? (validate-sudoku-grid
-									[[4 4 5 2 6 9 7 8 1]
-									 [6 8 2 5 7 1 4 9 3]
-									 [1 9 7 8 3 4 5 6 2]
-									 [8 2 6 1 9 5 3 4 7]
-									 [3 7 4 6 8 2 9 1 5]
-									 [9 5 1 7 4 3 6 2 8]
-									 [5 1 9 3 2 6 8 7 4]
-									 [2 4 8 9 5 7 1 3 6]
-									 [7 6 3 4 1 8 2 5 9]])))))
+ (testing "correct grid"
+	(is (true? (validate-sudoku-grid
+							[[4 3 5 2 6 9 7 8 1]
+							 [6 8 2 5 7 1 4 9 3]
+							 [1 9 7 8 3 4 5 6 2]
+							 [8 2 6 1 9 5 3 4 7]
+							 [3 7 4 6 8 2 9 1 5]
+							 [9 5 1 7 4 3 6 2 8]
+							 [5 1 9 3 2 6 8 7 4]
+							 [2 4 8 9 5 7 1 3 6]
+							 [7 6 3 4 1 8 2 5 9]]))))
+ (testing "incorrect grid"
+	(is (false? (validate-sudoku-grid
+							 [[4 4 5 2 6 9 7 8 1]
+								[6 8 2 5 7 1 4 9 3]
+								[1 9 7 8 3 4 5 6 2]
+								[8 2 6 1 9 5 3 4 7]
+								[3 7 4 6 8 2 9 1 5]
+								[9 5 1 7 4 3 6 2 8]
+								[5 1 9 3 2 6 8 7 4]
+								[2 4 8 9 5 7 1 3 6]
+								[7 6 3 4 1 8 2 5 9]])))))
